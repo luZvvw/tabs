e53bf38772c8cfe8f1d04590f2a8c6ce23e00aca
Александр Ишуткин <sanyaishu2.0@gmail.com>
2024-12-13 09:22:34 +0500

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,13 @@
+let items = document.querySelectorAll('#first ul:first-child li')
+let contents = document.querySelectorAll('#first ul:last-child li')
+let count = item.length
+
+for(let i = 0; 1 < items.length; i++) {
+    items[i].addEventListener('click', () => {
+
+        removeActive(items)
+        removeActive(contents)
+
+        items[i].classList+
+    })
+}
